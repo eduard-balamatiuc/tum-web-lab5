c81169ab8753c32271191bea8f08c3ee4c96e715
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-04-02 16:43:56 +0300

    feat: add url fetching function

--- src/go2web/main.py
+++ src/go2web/main.py
@@ -1,4 +1,8 @@
-import argparse
+import argparse 
+
+
+def fetch_url(url):
+    return "URL Fetched"
 
 
 def main():
@@ -10,13 +14,23 @@ def main():
     parser.add_argument(
         "-u",
         "--url",
+        help="URL to fetch",
     )
     parser.add_argument(
         "-s",
         "--search-term",
+        help="Term to search for",
     )
 
     args = parser.parse_args()
+
+    if args.url:
+        fetch_url(args.url)
+    elif args.search_term:
+        print("--search-term is not yet available")
+    else:
+        parser.print_help()
+
 
 if __name__=="__main__":
     main()
