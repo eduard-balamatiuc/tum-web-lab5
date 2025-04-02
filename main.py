9bd5c8f9500519127f1818dc51700c3dda30caec
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-04-02 14:29:29 +0300

    feat: create the parser using the argparse library

---
+++ main.py
@@ -0,0 +1,7 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="Go2Web",
+    description="Command Line Program for HTTP requests",
+    epilog="use the -h anytime you need some help",
+)
