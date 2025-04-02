53333c7036f5479db1dce11f38ecfa05a165783d
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-04-02 14:50:20 +0300

    feat: wrap the main file logic into a function

--- src/go2web/main.py
+++ src/go2web/main.py
@@ -1,17 +1,22 @@
 import argparse
 
-parser = argparse.ArgumentParser(
-    prog="Go2Web",
-    description="Command Line Program for HTTP requests",
-    epilog="use the -h anytime you need some help",
-)
 
-parser.add_argument(
-    "-u",
-    "--url",
-)
+def main():
+    parser = argparse.ArgumentParser(
+        prog="Go2Web",
+        description="Command Line Program for HTTP requests",
+        epilog="use the -h anytime you need some help",
+    )
+    parser.add_argument(
+        "-u",
+        "--url",
+    )
+    parser.add_argument(
+        "-s",
+        "--search-term",
+    )
 
-parser.add_argument(
-    "-s",
-    "--search-term",
-)
+    args = parser.parse_args()
+
+if __name__=="__main__":
+    main()
